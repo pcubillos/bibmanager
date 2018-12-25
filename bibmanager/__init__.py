94faf5f3035995bee9fa7c9383078810eb06ef24
pcubillos <pcubillos@knights.ucf.edu>
2018-12-25 20:15:09 -0300

    Added bib_manager.py functions into bibmanager __init__.py package.

--- bibmanager/__init__.py
+++ bibmanager/__init__.py
@@ -1,10 +1,13 @@
 # Copyright (c) 2018 Patricio Cubillos and contributors.
 # bibmanager is open-source software under the MIT license (see LICENSE).
 
-__all__ = []
-
 import os
 from . import VERSION as ver
+from .bib_manager import *
+
+from .bib_manager import __all__ as bm_all
+
+__all__ = bm_all
 
 __version__ = "{:d}.{:d}.{:d}".format(ver.BM_VER, ver.BM_MIN, ver.BM_REV)
 
