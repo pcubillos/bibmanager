4468e8a6554a9387bbffdc44113d6f28a211c4d9
pcubillos <patricio.cubillos@oeaw.ac.at>
2018-12-30 17:52:12 -0300

    Split cm.set_key() function into set() and help(). Added examples to set() docstring.

--- bibmanager/config_manager.py
+++ bibmanager/config_manager.py
@@ -27,7 +27,7 @@ def reset():
   shutil.copy(ROOT+'config', HOME+'config')
 
 
-def set_key(key, value=None):
+def set(key, value):
   """
   Set the value of a bibmanager config key.
 
@@ -37,7 +37,34 @@ def set_key(key, value=None):
      bibmanager config key to set.
   value: String
      Value to set for input key.
-     If value is None, display the key's help information on the prompt.
+
+  Examples
+  --------
+  >>> import config_manager as cm
+  >>> # Update text editor:
+  >>> cm.set('text_editor', 'vim')
+  text_editor updated to: vim.
+
+  >>> # Invalid bibmanager key:
+  >>> cm.set('styles', 'arduino')
+  ValueError: 'styles' is not a valid bibmanager config key.
+  The available keys are: ['style', 'text_editor', 'paper', 'adstoken']
+
+  >>> # Attempt to set an invalid style:
+  >>> cm.set('style', 'fake_style')
+  ValueError: 'fake_style' is not a valid style option.  Available options are:
+    default, emacs, friendly, colorful, autumn, murphy, manni, monokai, perldoc,
+    pastie, borland, trac, native, fruity, bw, vim, vs, tango, rrt, xcode, igor,
+    paraiso-light, paraiso-dark, lovelace, algol, algol_nu, arduino,
+    rainbow_dash, abap
+
+  >>> # Attempt to set an invalid command for text_editor:
+  >>> cm.set('text_editor', 'my_own_editor')
+  ValueError: 'my_own_editor' is not a valid text editor.
+
+  >>> # Beware, one can still set a valid command that doesn't edit text:
+  >>> cm.set('text_editor', 'less')
+  text_editor updated to: less.
   """
   config = configparser.ConfigParser()
   config.read(HOME+'config')
@@ -46,30 +73,39 @@ def set_key(key, value=None):
                        "\nThe available keys are: {}".
                        format(key, config.options('BIBMANAGER')))
 
-  # Set value:
-  if value is not None:
-      # Check for exceptions:
-      if key == 'style' and value not in STYLE_MAP.keys():
-          raise ValueError("'{:s}' is not a valid style option.  "
-              "Available options are:\n{:s}".format(value, styles))
-      # The code identifies invalid commands, but cannot assure that a
-      # command actually applies to a text file.
-      if key == 'text_editor' and shutil.which(value) is None:
-          raise ValueError("'{:s}' is not a valid text editor.".format(value))
+  # Check for exceptions:
+  if key == 'style' and value not in STYLE_MAP.keys():
+      raise ValueError("'{:s}' is not a valid style option.  "
+          "Available options are:\n{:s}".format(value, styles))
 
-      config.set('BIBMANAGER', key, value)
-      with open(HOME+'config', 'w') as configfile:
-          config.write(configfile)
-      return
+  # The code identifies invalid commands, but cannot assure that a
+  # command actually applies to a text file.
+  if key == 'text_editor' and shutil.which(value) is None:
+      raise ValueError("'{:s}' is not a valid text editor.".format(value))
 
-  # Display helps (value is None):
+  # Set value if there were no exceptions raised:
+  config.set('BIBMANAGER', key, value)
+  with open(HOME+'config', 'w') as configfile:
+      config.write(configfile)
+  print('{:s} updated to: {:s}.'.format(key, value))
+
+
+def help(key):
+  """
+  Display help information.
+
+  Parameters
+  ----------
+  key: String
+     A bibmanager config key.
+  """
   if key == 'style':
       print("The 'style' key sets the color-syntax style of displayed BibTeX "
             "entries.\nThe default style is 'autumn'.  "
             "The current style is '{:s}'.\n\n"
             "Available options are:\n{:s}\n"
             "See http://pygments.org/demo/6780986/ for a demo of the style "
-            "options.".format(config.get('BIBMANAGER',key), styles))
+            "options.".format(get(key), styles))
 
   elif key == 'text_editor':
       print("The 'text_editor' key sets the text editor to use when editing "
@@ -77,21 +113,24 @@ def set_key(key, value=None):
             "default, bibmanager\nuses the OS-default text editor.\n\n"
             "Typical text editors are: emacs, vim, gedit.\n"
             "To set the OS-default editor set text_editor to 'default'.\n"
-            "Note that aliases defined in the .bash are not accessible.\n"
-            "The current text editor is '{:s}'.".
-            format(config.get('BIBMANAGER',key)))
+            "Note that aliases defined in the .bash are not accessible.\n\n"
+            "The current text editor is '{:s}'.".format(get(key)))
 
   elif key == 'paper':
       print("The 'paper' key sets the default paper format for latex "
             "compilation outputs\n(not for pdflatex, which is automatic).  "
             "Typical options are 'letter'\n(e.g., for ApJ articles), or "
-            "'A4' (e.g., for A&A).\n\nCurrent value is: '{:s}'.".
-            format(config.get('BIBMANAGER',key)))
+            "'A4' (e.g., for A&A).\n\nThe current paper format is: '{:s}'.".
+            format(get(key)))
 
   elif key == 'adstoken':
       print("The 'adstoken' key sets the ADS token required for ADS requests.\n"
             "To obtain a token follow the two steps described here:\n"
             "  https://github.com/adsabs/adsabs-dev-api#access")
+  else:
+      # Call get() to trigger exception:
+      get(key)
+
 
 def get(key):
   """
@@ -118,7 +157,7 @@ def get(key):
   config = configparser.ConfigParser()
   config.read(HOME+'config')
   if not config.has_option('BIBMANAGER', key):
-      raise ValueError("No key '{:s}' in bibmanager config file."
+      raise ValueError("'{:s}' is not a valid bibmanager config key."
                        "\nThe available keys are: {}".
                        format(key, config.options('BIBMANAGER')))
   return config.get('BIBMANAGER', key)
