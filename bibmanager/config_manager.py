f5cc7517551e4c5d9a997bf4c3941a945f3403e9
pcubillos <patricio.cubillos@oeaw.ac.at>
2018-12-30 13:31:18 -0300

    Implemented cm.set_key() function to set bibmanager config values. Display help when input value is None (implemented for 'style' and 'paper').

--- bibmanager/config_manager.py
+++ bibmanager/config_manager.py
@@ -6,12 +6,16 @@ __all__ = ['reset']
 import os
 import shutil
 import configparser
+import textwrap
+from pygments.styles import STYLE_MAP
 
 
 # Set these as a unique constant in the pakage (in __init__?)
 ROOT = os.path.dirname(os.path.realpath(__file__)) + '/'
 HOME = os.path.expanduser("~") + "/.bibmanager/"
 
+styles = textwrap.fill(", ".join(style for style in iter(STYLE_MAP)),
+                       width=79, initial_indent="  ", subsequent_indent="  ")
 
 def reset():
   """
@@ -29,8 +33,42 @@ def set_key(key, value=None):
   """
   config = configparser.ConfigParser()
   config.read(bm_config)
-  for key, value in config['BIBMANAGER'].items():
-      print(key, value)
+  if not config.has_option('BIBMANAGER', key):
+      raise ValueError("'{:s}' is not a valid bibmanager config key."
+                       "\nThe available keys are: {}".
+                       format(key, config.options('BIBMANAGER')))
+
+  # Set value:
+  if value is not None:
+      config.set('BIBMANAGER', key, value)
+      if key == 'style' and value not in STYLE_MAP.keys():
+          raise ValueError("'{:s}' is not a valid style option.  "
+              "Available options are:\n{:s}".format(value, styles))
+
+      with open(HOME+'config', 'w') as config:
+          config.write(config)
+      return
+
+  # Display helps (value is None):
+  if key == 'style':
+      print("The 'style' key sets the color-syntax style of displayed BibTeX "
+            "entries.\nDefault style is 'autumn'.  Current style is '{:s}'. "
+            "\n\nAvailable options are:\n{:s}\n"
+            "See http://pygments.org/demo/6780986/ for a demo of the style "
+            "options.".format(config.get('BIBMANAGER',key), styles))
+
+  elif key == 'text_editor':
+      pass
+
+  elif key == 'paper':
+      print("The 'paper' key sets the default paper format for latex "
+            "compilation outputs\n(not for pdflatex, which is automatic).  "
+            "Typical options are 'letter'\n(e.g., for ApJ articles), or "
+            "'A4' (e.g., for A&A).\n\nCurrent value is: '{:s}'.".
+            format(config.get('BIBMANAGER',key)))
+
+  elif key == 'adstoken':
+      pass
 
 
 def get_key(key=None):
@@ -49,7 +87,7 @@ def get_key(key=None):
   if key is not None:
       if not config.has_option('BIBMANAGER', key):
           raise ValueError("No key '{:s}' in bibmanager config file."
-                           "\nAvailable keys are: {}".
+                           "\nThe available keys are: {}".
                            format(key, config.options('BIBMANAGER')))
       print("{:s}: {:s}".format(key, config.get('BIBMANAGER', key)))
   else:
