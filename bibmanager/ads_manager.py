d262c2d6a813e5fec42d09780b9864d755090d74
pcubillos <patricio.cubillos@oeaw.ac.at>
2019-01-01 13:58:03 -0300

    Implemented am.display() to show a sub-set of entries on the prompt. Renamed rows to cache_rows in am.search() to distinguish from ads_diplay rows.

--- bibmanager/ads_manager.py
+++ bibmanager/ads_manager.py
@@ -1,21 +1,53 @@
+# Copyright (c) 2018-2019 Patricio Cubillos and contributors.
+# bibmanager is open-source software under the MIT license (see LICENSE).
+
+__all__ = ['search', 'display']
+
+import os
 import json
 import requests
 import urllib
 import textwrap
+import pickle
 
 import config_manager as cm
+from utils import BM_CACHE, BOLD, END, ignored, parse_name, get_authors
+
 
 # FINDME: Is to possible to check a token is valid?
-token = 'Bearer ' + cm.get('adstoken')
-BOLD = '\033[1m'
-END  = '\033[0m'
+token = 'Bearer ' + cm.get('ads_token')
 
 ADSQUERRY = "https://api.adsabs.harvard.edu/v1/search/query?"
 ADSEXPORT = "https://api.adsabs.harvard.edu/v1/export/bibtex"
 ADSURL    = "https://ui.adsabs.harvard.edu//#abs/"
 
 
-def search(querry, start=0, rows=500, sort='pubdate+desc'):
+def manager(querry=None):
+  """
+  A manager, it doesn't really do anything, it just delegates.
+  """
+  if querry is None:
+      results, querry, start, index, nmatch = read_cache()
+      last = start + len(results)
+      if last < nmatch and index + rows > last:
+          new_results, nmatch = search(querry, start=last)
+          results = results[index-start:] + new_results
+          start = index
+          last = start + len(results)
+  else:
+      start = 0
+      index = start
+      results, nmatch = search(querry, start=start)
+  display(results, start, index, rows, nmatch)
+  index += rows
+  if index >= nmatch:
+      with ignored:
+          os.remove(BM_CACHE)
+  else:
+      cache(results, querry, start, index, nmatch)
+
+
+def search(querry, start=0, cache_rows=200, sort='pubdate+desc'):
   """
   Make a querry from ADS.
 
@@ -35,7 +67,7 @@ def search(querry, start=0, rows=500, sort='pubdate+desc'):
   -------
   results: List of dicts
      Querry outputs between indices start and start+rows.
-  nentries: Integer
+  nmatch: Integer
      Total number of entries matched by the querry.
 
   Resources
@@ -47,10 +79,11 @@ def search(querry, start=0, rows=500, sort='pubdate+desc'):
 
   Examples
   --------
+  >>> import ads_manager as am
   >>> # Search entries by author (note the need for double quotes,
   >>> # otherwise, the search might produce bogus results):
   >>> querry = 'author:"cubillos, p"'
-  >>> results, nentries = am.search(querry)
+  >>> results, nmatch = am.search(querry)
   >>> # Search entries by first author:
   >>> querry = 'author:"^cubillos, p"'
   >>> # Combine search by first author and year:
@@ -61,15 +94,15 @@ def search(querry, start=0, rows=500, sort='pubdate+desc'):
   >>> querry = 'author:"^cubillos, p" property:refereed'
 
   >>> # Attempt with invalid token:
-  >>> results, nentries = am.search(querry)
+  >>> results, nmatch = am.search(querry)
   ValueError: Invalid ADS request: Unauthorized, check you have a valid ADS token.
   >>> # Attempt with invalid querry ('properties' instead of 'property'):
-  >>> results, nentries = am.search('author:"^cubillos, p" properties:refereed')
+  >>> results, nmatch = am.search('author:"^cubillos, p" properties:refereed')
   ValueError: Invalid ADS request:
   org.apache.solr.search.SyntaxError: org.apache.solr.common.SolrException: undefined field properties
   """
   querry = urllib.parse.quote(querry)
-  r = requests.get(f'{ADSQUERRY}q={querry}&start={start}&rows={rows}'
+  r = requests.get(f'{ADSQUERRY}q={querry}&start={start}&rows={cache_rows}'
                    f'&sort={sort}&fl=title,author,year,bibcode,pub',
                    headers={'Authorization': token})
   resp = r.json()
@@ -79,33 +112,91 @@ def search(querry, start=0, rows=500, sort='pubdate+desc'):
                             "check you have a valid ADS token.")
       raise ValueError(f"Invalid ADS request:\n{resp['error']['msg']}.")
 
-  nentries = resp['response']['numFound']
-  results  = resp['response']['docs']
+  nmatch  = resp['response']['numFound']
+  results = resp['response']['docs']
 
-  return results, nentries
+  return results, nmatch
 
 
-def display(results, start, nentries, short=True):
+def cache(results, istart, index, nmatch):
+  """
+  Cache the current set of results returned by am.search(), the index
+  of the last entry shown to the user, and the total number of entries
+  matched by the search call.
+
+  Parameters
+  ----------
+  querry: String
+     I guess I don't need this
+  results: List of dicts
+     The output list of entries returned by a am.search() call.
+  nmatch: Integer
+     Total number of entries matched by the querry/search.
+  """
+  # New cache:
+  if istart == 0:
+      with open(BM_CACHE, 'wb') as handle:
+          pickle.dump([results, istart, index, nmatch], handle,
+                      protocol=pickle.HIGHEST_PROTOCOL)
+  # Append to existing cache:
+  else:
+      # load previous cache
+      with open(BM_CACHE, 'rb') as handle:
+          old_results, dummy, old_index, dummy = pickle.load(handle)
+      # keep from index to end
+      # prepend to results
+      results = old_results[index:] + results
+      # save
+      with open(BM_CACHE, 'wb') as handle:
+          pickle.dump([results, index, index, nmatch], handle,
+                      protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def display(results, start, index, rows, nmatch, short=True):
   """
   Show on the prompt a list of entries from an ADS search.
+
+  Parameters
+  ----------
+  results: List of dicts
+     Subset of entries returned by a querry.
+  start: Integer
+     Index assigned to first entry in results.
+  index: Integer
+     First index to display.
+  rows: Integer
+     Number of entries to display.
+  nmatch: Integer
+     Total number of entries corresponding to querry (not necessarily
+     the number of entries in results).
+  short: Bool
+     Format for author list. If True, truncate with 'et al' after
+     the second author.
+
+  Examples
+  --------
+  >>> import ads_manager as am
+  >>> start = index = 0
+  >>> querry = 'author:"^cubillos, p" property:refereed'
+  >>> results, nmatch = am.search(querry, start=start)
+  >>> display(results, start, index, rows, nmatch)
   """
-  wrap_kw = {'width':80, 'subsequent_indent':"   "}
-  for result in results:
-      title = textwrap.fill(f"Title: {result['title'][0]}, {result['year']}",
-          **wrap_kw)
-      author_list = [bm.parse_name(author) for author in result['author']]
-      authors = textwrap.fill(f"Authors: {bm.get_authors(author_list, short)}",
+  wrap_kw = {'width':80, 'subsequent_indent':"    "}
+  last = start + len(results)
+  for result in results[index-start:index-start+rows]:
+      title = textwrap.fill(f"Title: {result['title'][0]}", **wrap_kw)
+      author_list = [parse_name(author) for author in result['author']]
+      authors = textwrap.fill(f"Authors: {get_authors(author_list, short)}",
            **wrap_kw)
       adsurl = f"adsurl: {ADSURL}{result['bibcode']}"
       bibcode = f"\n{BOLD}bibcode{END}: {result['bibcode']}"
       print(f"\n{title}\n{authors}\n{adsurl}{bibcode}")
-  if start+len(results) < nentries:
+  if index + rows < nmatch:
       more = "  To show the next set, execute:\nbibm ads-search"
   else:
       more = ""
-  if rows < nentries:
-      print(f"\nShowing entries {start}--{start+len(results)} out of "
-            f"{nentries} entries.{more}")
+  print(f"\nShowing entries {index+1}--{min(index+1+rows, nmatch)} out of "
+        f"{nmatch} entries matched.{more}")
 
 
 def get_bibtex():
