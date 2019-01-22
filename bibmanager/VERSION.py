84cca32b49b76f3988ce4939650013dbcf11a280
Patricio Cubillos <patricio.cubillos@oeaw.ac.at>
2019-01-22 18:52:31 +0100

    Bumped bibmanager version to 1.0.0. This next merge closes #6 And completes the tasks for first release (exclamation mark)

--- bibmanager/VERSION.py
+++ bibmanager/VERSION.py
@@ -2,6 +2,6 @@
 # bibmanager is open-source software under the MIT license (see LICENSE).
 
 # bibmanager Version:
-BM_VER =  0  # Major version
-BM_MIN =  6  # Minor version
+BM_VER =  1  # Major version
+BM_MIN =  0  # Minor version
 BM_REV =  0  # Revision
